7f57743e64958177a8d46a4389cbd7afa2fcbb02
Mateo Gonzalez <146020964+MateoGonzalez12@users.noreply.github.com>
2026-04-29 18:35:27 -0500

    Update application.py

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ def home():
         <body style="font-family: Arial; text-align: center; margin-top: 50px;">
             <h1>🚀 Mi App está funcionando</h1>
             <p>Desplegada en Elastic Beanstalk</p>
-            <p>Versión 3</p>
+            <p>Versión 4</p>
             <a href="/health">Ver estado</a>
         </body>
     </html>
